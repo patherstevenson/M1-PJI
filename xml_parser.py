63be14e689ed2f41c933247fd71bc649c2c6fc50
pather <stevenson.pather.etu@univ-lille.fr>
2023-05-05 15:51:54 +0200

    xml_parser

---
+++ xml_parser.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def parse_XML(xml_file):
+    """Parse the input XML file and store the result in a pandas
+    DataFrame
+    """
+
+    xml_data = open(xml_file, 'r').read() 
+    root = ET.XML(xml_data) 
+
+    bndbox = []
+
+    for obj in root.findall("object"):
+        bndbox.append([obj.find("name").text,
+                       int(obj.find("bndbox").find("xmin").text),
+                       int(obj.find("bndbox").find("ymin").text),
+                       int(obj.find("bndbox").find("xmax").text),
+                       int(obj.find("bndbox").find("ymax").text)
+                      ])
+
+    out_df = pd.DataFrame(bndbox, columns=["name","xmin","ymin","xmax","ymax"])
+
+    return out_df
